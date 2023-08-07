f3f97db457aa783ee2630fb1d679fa402eebed72
Chenxyan99 <251601918@qq.com>
2023-08-07 17:27:23 +0800

    uno

--- shaders/cp2/basic_lighting.fs
+++ shaders/cp2/basic_lighting.fs
@@ -5,19 +5,43 @@ in vec3 Normal;
 in vec3 FragPos;
 
 uniform vec3 lightPos;
-uniform vec3 lightColor;
-uniform vec3 objectColor;
+uniform vec3 viewPos;
+struct Material {
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+    float shininess;
+};
+
+uniform Material material;
+
+struct Light {
+    vec3 Position;
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+uniform Light light;
 
 void main()
 {
-    float ambientStrength = 0.1;
-    vec3 ambient = ambientStrength * lightColor;
+    // 环境光
+    vec3 ambient = light.ambient * material.ambient;
 
+    //  漫反射
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(lightPos - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = diff * lightColor;
+    vec3 diffuse = (diff * material.diffuse) * light.diffuse;
 
-    vec3 result = (ambient + diffuse) * objectColor;
+    // 镜面反射
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);    
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    vec3 specular = (material.specular * spec) * light.specular;
+
+
+    vec3 result = ambient + diffuse + specular;
     FragColor = vec4(result, 1.0);
 }
